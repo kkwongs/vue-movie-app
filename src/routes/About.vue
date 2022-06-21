761f7d52be420d979d35e64c112b394c36b69a26
kkwongs <kihyun5741@gmail.com>
2022-06-21 17:59:54 +0900

    Create project

---
+++ src/routes/About.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="about">
+    <div class="photo">
+      <Loader
+        v-if="imageLoading"
+        absolute />
+      <img
+        :src="image"
+        :alt="name" />
+    </div>
+    <div class="name">
+      {{ name }}
+    </div>
+    <div>{{ email }}</div>
+    <a
+      href="https://kkwongs.github.io/portfolio/"
+      target="_blank">
+      {{ portfolio }}
+    </a>
+    <div>{{ phone }}</div>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import Loader from '~/components/Loader'
+
+export default {
+  components: {
+    Loader
+  },
+  data() {
+    return {
+      imageLoading: true
+    }
+  },
+  computed: {
+    ...mapState('about', [
+      'image',
+      'name',
+      'email',
+      'portfolio',
+      'phone'
+    ])
+  },
+  mounted() {
+    this.init()
+  },
+  methods: {
+    async init() {
+      await this.$loadImage(this.image)
+      this.imageLoading = false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.about {
+  text-align: center;
+  .photo {
+    width: 250px;
+    height: 250px;
+    margin: 40px auto 20px;
+    padding: 30px;
+    border: 10px solid $gray-300;
+    border-radius: 50%;
+    box-sizing: border-box;
+    position: relative;
+    img {
+      width: 100%;
+      border-radius: 50%;
+    }
+  }
+  .name {
+    font-size: 40px;
+    font-family: "Oswald", sans-serif;
+    margin-bottom: 20px;
+  }
+}
+</style>
---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+export default {
+  namespaced: true,
+  state: () => ({
+    name: 'KKWONGS',
+    email: 'kihyun5741@gmail.com',
+    portfolio: 'https://kkwongs.github.io/portfolio/',
+    phone: '+82-10-6512-5741',
+    image: 'https://avatars.githubusercontent.com/u/43363739?s=400&u=8ec11c2205b78aa172c3c62057d999925f88372d&v=4'
+  })
+}

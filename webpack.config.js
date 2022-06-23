ca4188ab75bbdfe62541491da8db5ef2be858875
kkwongs <kihyun5741@gmail.com>
2022-06-23 14:41:20 +0900

    Add Netlify functions & Modify server port number

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const { VueLoaderPlugin } = require('vue-loader');
+const Dotenv = require('dotenv-webpack')
 
 module.exports = {
   resolve: {
@@ -64,9 +65,11 @@ module.exports = {
       patterns: [{ from: 'static' }],
     }),
     new VueLoaderPlugin(),
+    new Dotenv()
   ],
 
   devServer: {
     host: 'localhost',
+    port: 8079
   },
 };
